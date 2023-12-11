8dd0ed0b4acc36015a33945dd1a7c147d1cdda31
ihidalgoGit <151981188+ihidalgoGit@users.noreply.github.com>
2023-12-10 20:36:12 -0600

    Grabacion de Reuniones del Projecto Pokemon
    This Project was build in Gradle 8.5 and is it to be run with Java 17 it contains the code of the Pokemon called. "personitaPoke" created by Israel Hidalgo Bonilla  for purpose of the construction and maintenance class of CENFOTEC

---
+++ PokemonIsrav4/pokemonIsra/src/test/java/com/cenfotec/pokemonisra/PokemonIsraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.pokemonisra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonIsraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
